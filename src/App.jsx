5e396c42552b2cbbfb632867da006bfa91759451
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-24 12:44:15 -0300

    feat: add Newscard component and his styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Row from './components/containers/Row';
 import Layout from './components/layouts/layout';
+import { NewsCard } from './components/containers/Cards';
 
 function App() {
 	return (
 		<Layout>
-			<Row size="row-md" title="Productos Destacados"></Row>
+			<Row cols="4" title="Productos Destacados">
+				<NewsCard />
+			</Row>
 		</Layout>
 	);
 }
---
+++ src/components/containers/Cards.jsx
@@ -0,0 +1,184 @@
+export function NewsCard() {
+	return (
+		<div className="product">
+			<div className="prodInfo">
+				<img
+					src="https://cdn.motor1.com/images/mgl/RqgR3K/s1/mazda-cx-60-2022.jpg"
+					alt=""
+				/>
+				<div className="prodData">
+					<h3 className="name">lorem ipsum dolor sit amet consecteur</h3>
+					<div className="rate">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+								fill="#FFD100"
+								stroke="#FFD100"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+								fill="#FFD100"
+								stroke="#FFD100"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+								fill="#FFD100"
+								stroke="#FFD100"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+								fill="#FFD100"
+								stroke="#FFD100"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="24"
+							height="24"
+							viewBox="0 0 24 24"
+							fill="none"
+						>
+							<path
+								d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+								fill="#FFD100"
+								stroke="#FFD100"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</svg>
+						<span className="numReviews">199</span>
+					</div>
+					<p className="id">25</p>
+					<p className="listPrice">9999</p>
+				</div>
+			</div>
+			<p className="price">6900</p>
+			<div className="btn">
+				<button>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+					>
+						<g clip-path="url(#clip0_86_1191)">
+							<path
+								d="M16.5 3H1.5V16H16.5V3Z"
+								stroke="black"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+							<path
+								d="M16.5 8H20.5L23.5 11V16H16.5V8Z"
+								stroke="black"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+							<path
+								d="M6 21C7.38071 21 8.5 19.8807 8.5 18.5C8.5 17.1193 7.38071 16 6 16C4.61929 16 3.5 17.1193 3.5 18.5C3.5 19.8807 4.61929 21 6 21Z"
+								stroke="black"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+							<path
+								d="M19 21C20.3807 21 21.5 19.8807 21.5 18.5C21.5 17.1193 20.3807 16 19 16C17.6193 16 16.5 17.1193 16.5 18.5C16.5 19.8807 17.6193 21 19 21Z"
+								stroke="black"
+								stroke-width="2"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+							/>
+						</g>
+						<defs>
+							<clipPath id="clip0_86_1191">
+								<rect
+									width="24"
+									height="24"
+									fill="white"
+									transform="translate(0.5)"
+								/>
+							</clipPath>
+						</defs>
+					</svg>
+				</button>
+				<button>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="25"
+						height="24"
+						viewBox="0 0 25 24"
+						fill="none"
+					>
+						<path
+							d="M6.5 2L3.5 6V20C3.5 20.5304 3.71071 21.0391 4.08579 21.4142C4.46086 21.7893 4.96957 22 5.5 22H19.5C20.0304 22 20.5391 21.7893 20.9142 21.4142C21.2893 21.0391 21.5 20.5304 21.5 20V6L18.5 2H6.5Z"
+							stroke="black"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+						<path
+							d="M3.5 6H21.5"
+							stroke="black"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+						<path
+							d="M16.5 10C16.5 11.0609 16.0786 12.0783 15.3284 12.8284C14.5783 13.5786 13.5609 14 12.5 14C11.4391 14 10.4217 13.5786 9.67157 12.8284C8.92143 12.0783 8.5 11.0609 8.5 10"
+							stroke="black"
+							stroke-width="2"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+					</svg>
+				</button>
+			</div>
+		</div>
+	);
+}
