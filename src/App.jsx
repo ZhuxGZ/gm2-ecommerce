c9bcbc69e60f21819c4601b4504c1f20eb26ad49
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-31 13:03:52 -0300

    feat: add stars and reviews values in the json

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import Row from './components/containers/Row';
 import Layout from './components/layouts/layout';
-import { NewsCard, ProdCard } from './components/containers/Cards';
 
 function App() {
 	return (
 		<Layout>
-			<Row col="4" title="Productos Destacados">
-				<ProdCard />
-				<ProdCard />
-				<ProdCard />
-				<ProdCard />
-			</Row>
-			<Row col="4" title="Productos Destacados">
-				<ProdCard />
-				<ProdCard />
-				<ProdCard />
-				<ProdCard />
-			</Row>
+			<Row col="4" title="Productos Destacados" />
+			<Row col="4" title="Productos en Oferta" discountRow />
+			<Row col="2" title="Novedades" newsRow />
 		</Layout>
 	);
 }
