e099aec66a605caa03b14f997ae78a8d41e2e9bd
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-31 13:03:10 -0300

    feat: Add iteration to display the products

--- src/components/containers/Row.jsx
+++ src/components/containers/Row.jsx
@@ -1,8 +1,79 @@
-export default function Row({ title, col, children }) {
+import { useEffect, useState } from 'react';
+import { NewsCard, ProdCard } from './Cards';
+
+export default function Row({ title, col, discountRow, newsRow }) {
+	let productsCard;
+	const [products, setProducts] = useState([]);
+	const getData = async () => {
+		const res = await fetch('./src/assets/mock_data.json');
+		const data = await res.json();
+		console.log('get data');
+		setProducts(await data);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	let date = new Date();
+	let flag = 0;
+	if (discountRow) {
+		productsCard = products.map((product) => {
+			if (product.discount != 0 && flag < 4) {
+				flag++;
+				return (
+					<ProdCard
+						key={product.id}
+						id={product.id}
+						title={product.name}
+						price={product.price}
+						discount={product.discount}
+					/>
+				);
+			}
+		});
+	} else if (newsRow) {
+		productsCard = products.map((product) => {
+			if (flag < 2) {
+				let dateDiff = Math.abs(date - new Date(product.date.replace(/-/g, '/')));
+				console.log(dateDiff / 1000 / 60 / 60 / 24);
+				if (dateDiff / 1000 / 60 / 60 / 24 <= 7) {
+					flag++;
+					return (
+						<NewsCard
+							key={product.id}
+							id={product.id}
+							title={product.name}
+							price={product.price}
+							discount={product.discount}
+							stars={product.stars}
+							numReviews={product.reviews}
+						/>
+					);
+				}
+			}
+		});
+	} else {
+		productsCard = products.map((product) => {
+			flag++;
+			if (flag < 9) {
+				return (
+					<ProdCard
+						key={product.id}
+						id={product.id}
+						title={product.name}
+						price={product.price}
+						discount={product.discount}
+					/>
+				);
+			}
+		});
+	}
+
 	return (
 		<section>
 			<h2 className="title">{title}</h2>
-			<div className={`col col-${col}`}>{children}</div>
+			<div className={`col col-${col}`}>{productsCard}</div>
 		</section>
 	);
 }
