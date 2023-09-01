72207944ceb867e239e76329842c114a5678d13b
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-09-01 11:51:03 -0300

    feat: Add a component for every svg in the app

---
+++ src/components/containers/svg/addCart.jsx
@@ -0,0 +1,116 @@
+export const AddCartPlus = () => {
+	return (
+		<span style={{ display: 'flex', justifyContent: 'center' }}>
+			<svg
+				width="24"
+				height="26"
+				viewBox="0 0 24 26"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<g clipPath="url(#clip0_215_332)">
+					<path
+						d="M5.6857 1.63828L2.42856 5.97621V21.159C2.42856 21.7342 2.65733 22.2859 3.06455 22.6927C3.47178 23.0995 4.02409 23.328 4.59999 23.328H19.8C20.3759 23.328 20.9282 23.0995 21.3354 22.6927C21.7426 22.2859 21.9714 21.7342 21.9714 21.159V5.97621L18.7143 1.63828H5.6857Z"
+						stroke="white"
+						strokeWidth="2.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+					<path
+						d="M2.42856 5.97623H21.9714"
+						stroke="white"
+						strokeWidth="2.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+					<path
+						d="M16.5428 10.3141C16.5428 11.4646 16.0853 12.568 15.2708 13.3815C14.4564 14.195 13.3518 14.6521 12.2 14.6521C11.0482 14.6521 9.94355 14.195 9.12911 13.3815C8.31467 12.568 7.85712 11.4646 7.85712 10.3141"
+						stroke="white"
+						strokeWidth="2.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</g>
+				<defs>
+					<clipPath id="clip0_215_332">
+						<rect
+							width="22.8"
+							height="25.2143"
+							fill="white"
+							transform="translate(0.799988 0.0115356)"
+						/>
+					</clipPath>
+				</defs>
+			</svg>
+			<svg
+				style={{ alignSelf: 'center' }}
+				width="12"
+				height="11"
+				viewBox="0 0 12 11"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+			>
+				<g clipPath="url(#clip0_215_336)">
+					<path
+						d="M5.74286 1.68747V9.27887"
+						stroke="white"
+						strokeWidth="2.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+					<path
+						d="M1.94284 5.48312H9.54284"
+						stroke="white"
+						strokeWidth="2.5"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+					/>
+				</g>
+				<defs>
+					<clipPath id="clip0_215_336">
+						<rect
+							width="11.4"
+							height="10.5737"
+							fill="white"
+							transform="translate(0.0428467 0.331787)"
+						/>
+					</clipPath>
+				</defs>
+			</svg>
+		</span>
+	);
+};
+
+export const AddCart = () => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="25"
+			height="24"
+			viewBox="0 0 25 24"
+			fill="none"
+		>
+			<path
+				d="M6.5 2L3.5 6V20C3.5 20.5304 3.71071 21.0391 4.08579 21.4142C4.46086 21.7893 4.96957 22 5.5 22H19.5C20.0304 22 20.5391 21.7893 20.9142 21.4142C21.2893 21.0391 21.5 20.5304 21.5 20V6L18.5 2H6.5Z"
+				stroke="black"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M3.5 6H21.5"
+				stroke="black"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+			<path
+				d="M16.5 10C16.5 11.0609 16.0786 12.0783 15.3284 12.8284C14.5783 13.5786 13.5609 14 12.5 14C11.4391 14 10.4217 13.5786 9.67157 12.8284C8.92143 12.0783 8.5 11.0609 8.5 10"
+				stroke="black"
+				strokeWidth="2"
+				strokeLinecap="round"
+				strokeLinejoin="round"
+			/>
+		</svg>
+	);
+};
